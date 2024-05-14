3ea1f6672977be65ffc5368f96ee3d2df2a2245d
ndzywork998 <zhang.yi@pitayagames.com>
2024-05-14 17:33:32 +0800

    chore: 1

--- src/components/ArticleSelect.tsx
+++ src/components/ArticleSelect.tsx
@@ -44,7 +44,10 @@ const ArticleSelect = ({
         id="title"
         className="px-4 block w-[20rem] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         value={title}
-        onClick={() => setOpen(true)}
+        onClick={(e) => {
+          e.preventDefault();
+          setOpen(true);
+        }}
         placeholder="请选择"
       />
 
